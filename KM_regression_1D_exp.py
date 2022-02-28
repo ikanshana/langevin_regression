bf09d2d27410c73394bce82554ae3cdbd80490a1
Indra Kanshana <indra.kanshana@gmail.com>
2022-02-28 15:55:58 +0100

    KL weight set to 5

--- KM_regression_1D_exp.py
+++ KM_regression_1D_exp.py
@@ -125,7 +125,7 @@ fp = fpsolve.SteadyFP_reflectBC(N_bins, dx)
 
 #Optimisation parameters
 params = {"W": W, "KMc": KMc, "Xi0": Xi0, "N": N_bins, "p_hist": p_hist,
-          "kl_reg": 1,  "Nbr_iter_max": 1e5, "track": 0,
+          "kl_reg": 5,  "Nbr_iter_max": 1e5, "track": 0,
           "fp": fp, "afp": afp, "tau": stride*delta_t, "radial": False,
           "print_cost": True, "checkpoint_file": save_load,"Loss_parts" : True,
           "checkpoint_load": checkpoint_load}
